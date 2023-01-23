e479dba72c2540bfc23336491bed97ed8e0dcbb6
Sander Schnydrig <sander@pomino.ch>
2023-01-23 16:16:33 +0100

    tsconfig: target to ES5

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.project',
+  appName: 'ng13',
+  webDir: 'dist/ng13',
+  bundledWebRuntime: false
+};
+
+export default config;
