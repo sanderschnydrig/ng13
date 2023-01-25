54a3ce9b8d884749f86c63494f4fbdc2a88cb033
Sander Schnydrig <sander@pomino.ch>
2023-01-25 13:24:33 +0100

    capacitor: live reload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.project',
+  appId: 'io.ionic.starter',
   appName: 'ng13',
   webDir: 'dist/ng13',
   bundledWebRuntime: false
